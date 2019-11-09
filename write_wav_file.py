73ff46925d7f84d0f2c5a8ef3b8134df42daea1e
unknown <sohraubpazuki@gmail.com>
2019-11-09 21:57:34 +0000

    fixed name of output file, added comments

--- write_wav_file.py
+++ write_wav_file.py
@@ -9,14 +9,14 @@ Given sample rate (input and output), frame length, and the data array (1-D)
 
 
 def write_wav_file(file_name, data, sample_rate=48000, frame_length=2048):
-    if os.path.splitext(file_name)[1] != 'wav':
-        file_name = file_name.replace(os.path.splitext(file_name)[1], 'wav')
+    if os.path.splitext(file_name)[1].strip('.') != 'wav':
+        file_name = file_name.replace(os.path.splitext(file_name)[1].strip('.'), 'wav')
     data_dims = data.shape[0]
     if frame_length != 1:
         output_data = np.zeros((data_dims, len(data[0]) * frame_length))
-        for dim in range(data_dims):
+        for dim in range(data_dims):  # Looping through no. of channels (expect 1 for mono)
             for i in range(len(data[dim])):
-                for j in range(frame_length):
+                for j in range(frame_length):  # Looping through data array, repeating value for each frame
                     output_data[dim, (i*frame_length)+j] = data[dim][i]
     else:
         output_data = data
