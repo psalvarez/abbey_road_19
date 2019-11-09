40ec7842b96558fea7f15591f27f9f302271dbcc
Pedro Sanchez <pedsaalv@gmail.com>
2019-11-09 21:19:36 +0000

    I am a pedantic asshole

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
-import argparse
-import librosa
-import librosa.feature
 
 
 def load_file(in_file):
+    import librosa
     print("Loading file " + in_file)
     real_sr = librosa.get_samplerate(in_file)
     signal, sr = librosa.load(in_file, sr=real_sr)
@@ -13,6 +11,7 @@ def load_file(in_file):
 
 
 def extract_rms(signal, window=2048):
+    import librosa.feature
     print("Extracting RMS loudness...")
     # We don't want overlap, so frame_length == hop_length
     rms_vals = librosa.feature.rms(y=in_signal, frame_length=window, hop_length=window)
@@ -21,6 +20,7 @@ def extract_rms(signal, window=2048):
 
 
 def menu():
+    import argparse
     parser = argparse.ArgumentParser(description='Extract features from audio signal')
     parser.add_argument('-f', '--file', default='sounds/annoying_arp.wav')
     parser.add_argument('-l', '--frame-length', default=2048)
