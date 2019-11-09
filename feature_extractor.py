8f3f569d694b1fd0f71bd19eececaf5caacb9c16
Pedro Sanchez <pedsaalv@gmail.com>
2019-11-09 20:41:34 +0000

    Refactor load

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,11 +3,18 @@ import argparse
 import librosa
 import librosa.feature
 
+
+def load_file(in_file):
+    real_sr = librosa.get_samplerate(in_file)
+    signal, sr = librosa.load(in_file, sr=real_sr)
+    return signal, sr
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract features from audio signal')
     parser.add_argument('-f', '--file', default='sounds/annoying_arp.wav')
     args = parser.parse_args()
 
-    sr = librosa.get_samplerate(args.file)
-    signal, sr = librosa.load(args.file, sr=sr)
+    signal, sr = load_file(args.file) 
     print(sr)
+
